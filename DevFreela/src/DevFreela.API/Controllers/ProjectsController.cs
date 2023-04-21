ad71ba09c082b21e85db5542fb48a066fa669988
Brian Pontes <brian.robert16@hotmail.com>
2023-04-21 13:29:41 -0300

    finishing first module

---
+++ DevFreela/src/DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,69 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly OpeningTimeOption options;
+        public ProjectsController(IOptions<OpeningTimeOption> options)
+        {
+            this.options = options.Value;
+        }
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost()]
+        public IActionResult Post([FromBody] CreateProjectModel createProjectModel)
+        {
+            if (createProjectModel.title.Length > 50)
+                return BadRequest();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel createProjectModel)
+        {
+            if (createProjectModel.Description.Length > 200)
+                return BadRequest();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment([FromBody] CreateCommentModel createCommentModel)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela/src/DevFreela.API/Models/CreateProjectModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DevFreela.API.Models
+{
+    public class CreateProjectModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}

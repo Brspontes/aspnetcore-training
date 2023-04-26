dae5b40eec1e1a275cb69a21835ef9c9ce613fc2
Brian Pontes <brian.robert16@hotmail.com>
2023-04-26 19:37:54 -0300

    feat: created entities

---
+++ DevFreela/src/DevFreela.Core/Entities/Projects.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class Projects : BaseEntity
+    {
+        public Projects(string title, string description, int idClient, int idFreenlancer, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            IdClient = idClient;
+            IdFreenlancer = idFreenlancer;
+            TotalCost = totalCost;
+
+            CreatedAt = DateTime.Now;
+            Comments = new List<ProjectsComments>();
+            Status = ProjectStatusEnum.Created;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int IdClient { get; private set; }
+        public int IdFreenlancer { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public  List<ProjectsComments> Comments { get; private set; }
+    }
+}

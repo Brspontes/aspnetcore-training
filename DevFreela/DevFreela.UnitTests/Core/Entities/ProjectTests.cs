36085d00aa83d9a51b9696592aba42e3624fd3b2
Brian Pontes <brian.robert16@hotmail.com>
2023-10-03 13:42:39 -0300

    criando projeto de teste

---
+++ DevFreela/DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestProjectStartWorks()
+        {
+            var project = new Project("Project Title", "Description Project", 1, 1, 10000);
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}

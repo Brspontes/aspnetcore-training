a28c1e61526f28562f3b7024d1d130b28d1adfb9
Brian Pontes <brian.robert16@hotmail.com>
2023-10-11 13:38:31 -0300

    adding microservice payments

---
+++ DevFreelaPayments/DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Payments.API.Models;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public async Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            return true;
+        }
+    }
+}
